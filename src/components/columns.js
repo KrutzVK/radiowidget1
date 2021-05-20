a1fc76aa854a9fe37b961e74c58ce83283a9206f
KrutzVK <krutika95.kk@gmail.com>
2021-05-20 10:15:20 +0300

    Initial commit

---
+++ src/components/columns.js
@@ -0,0 +1,14 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Name',
+        accessor: 'radio_name'
+    },
+    {
+        Header: 'Frequency',
+        accessor: 'radio_frequency'
+    }
+]
